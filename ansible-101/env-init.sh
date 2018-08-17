f098bb06e2b2fd504db4b4dda4f3410213f4c1d1
Tomoaki Nakajima <powered.by.solaris@gmail.com>
2018-08-17 14:11:50 +0900

    test

--- ansible-101/env-init.sh
+++ ansible-101/env-init.sh
@@ -1,1 +1,3 @@
 git clone https://github.com/irixjp/katacoda-scenarios.git
+pwd > ~/test.txt
+bash ./katacoda-scenarios/ansible-101/tools/setup.sh
--- ansible-101/tools/setup.sh
+++ ansible-101/tools/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "setup hands-on environments"
-
-docker run -d --privileged --name node-1 -h node-1 -p 8081:80 centos:7 /sbin/init
-docker run -d --privileged --name node-2 -h node-2 -p 8082:80 centos:7 /sbin/init
-docker run -d --privileged --name node-3 -h node-3 -p 8083:80 centos:7 /sbin/init
+docker run -d --privileged --name control -h control -p 8081:80 centos:7 /sbin/init
+docker run -d --privileged --name node-1  -h node-1  -p 8081:80 centos:7 /sbin/init
+docker run -d --privileged --name node-2  -h node-2  -p 8082:80 centos:7 /sbin/init
+docker run -d --privileged --name node-3  -h node-3  -p 8083:80 centos:7 /sbin/init
