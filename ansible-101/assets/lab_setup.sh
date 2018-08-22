e84a5ed39aef204c559e29e21b53f4219a997f9b
Tomoaki Nakajima <powered.by.solaris@gmail.com>
2018-08-22 15:05:15 +0900

    test

--- ansible-101/assets/lab_setup.sh
+++ ansible-101/assets/lab_setup.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
-echo "setup hands-on environments"
-docker run -d --privileged --name node-1  -h node-1  -p 8081:80 centos:7 /sbin/init
-docker run -d --privileged --name node-2  -h node-2  -p 8082:80 centos:7 /sbin/init
-docker run -d --privileged --name node-3  -h node-3  -p 8083:80 centos:7 /sbin/init
 
-docker exec -it node-1  sh -c "yum install -y openssh-server && systemctl start sshd"
-docker exec -it node-2  sh -c "yum install -y openssh-server && systemctl start sshd"
-docker exec -it node-3  sh -c "yum install -y openssh-server && systemctl start sshd"
+yum install -y jq
+
+echo "setup hands-on environments"
+docker run -d --privileged --name node-1  -h node-1  -p 8081:80 irixjp/katacoda:latest /sbin/init
+docker run -d --privileged --name node-2  -h node-2  -p 8082:80 irixjp/katacoda:latest /sbin/init
+docker run -d --privileged --name node-3  -h node-3  -p 8083:80 irixjp/katacoda:latest /sbin/init
+
+docker exec -it node-1  sh -c "systemctl start sshd"
+docker exec -it node-2  sh -c "systemctl start sshd"
+docker exec -it node-3  sh -c "systemctl start sshd"
 
 docker exec -it node-1  sh -c "echo password | passwd --stdin root"
 docker exec -it node-2  sh -c "echo password | passwd --stdin root"
 docker exec -it node-3  sh -c "echo password | passwd --stdin root"
 
-docker inspect node-1  | jq ".[0].NetworkSettings.IPAddress"
-docker inspect node-2  | jq ".[0].NetworkSettings.IPAddress"
-docker inspect node-3  | jq ".[0].NetworkSettings.IPAddress"
+docker inspect node-1  | jq -r ".[0].NetworkSettings.IPAddress" 
+docker inspect node-2  | jq -r ".[0].NetworkSettings.IPAddress"
+docker inspect node-3  | jq -r ".[0].NetworkSettings.IPAddress"
