d353f0221dd43b424cdce22b248df14ff5677774
Tomoaki Nakajima <powered.by.solaris@gmail.com>
2021-10-05 14:31:34 +0900

    minor fix new 101

--- tools/kata_setup.sh
+++ tools/kata_setup.sh
@@ -1,9 +1,17 @@
 #!/bin/bash -ex
 
-source ./environments
+ANSIBLE_CORE=2.11.5
+ANSIBLE_LINT=5.2.0
+MOLECULE=
+YAML_LINT=1.26.3
+DOCKER_PY=5.0.0
+DOCKER_COLLECTION=1.8.0
+CRYPT_COLLECTION=1.7.1
+PYYAML==5.4.1
 
-apt remove -qyy ansible
+apt remove -qyy ansible python3-yaml
 pip install -U pip setuptools
+pip install -U --ignore-install PyYAML==${PYYAML}
 pip install ansible-core==${ANSIBLE_CORE} ansible-lint==${ANSIBLE_LINT} yamllint==${YAML_LINT} docker==${DOCKER_PY}
 hash -r
 ansible-galaxy collection install community.docker:${DOCKER_COLLECTION}
